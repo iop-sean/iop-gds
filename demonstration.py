9ebfdebd09cc90352ec8a9cab2f642863164fba7
iop-sean <sean.bommer.2014@uni.strath.ac.uk>
2021-01-21 12:17:20 +0000

    added if statement to plot options

--- demonstration.py
+++ demonstration.py
@@ -15,30 +15,28 @@ import iopgdstoolkit as iop
 
 # ===========================================================================================
 # ===========================================================================================
-
-cell = generate_layout_cell()
-cell.show()
+one_or_layout = 0
+if one_or_layout == 0:
+    cell = generate_layout_cell()
+    cell.show()
 
 # ===========================================================================================
 # ===========================================================================================
+else:
+    layout = GridLayout(region_layer_type=None, frame_layer=0,
+                        vertical_spacing=40, vertical_alignment=1,
+                        horizontal_spacing=100, horizontal_alignment=50)
 
+    for ii in range(0, 2):
+        layout.begin_new_row()
+        for jj in range(0, 2):
+            cell = generate_layout_cell()
+            layout.add_to_row(cell)
 
-"""
-layout = GridLayout(region_layer_type=None, frame_layer=0,
-                    vertical_spacing=40, vertical_alignment=1,
-                    horizontal_spacing=100, horizontal_alignment=50)
+    layout_cell, mapping = layout.generate_layout()
 
-for ii in range(0, 2):
-    layout.begin_new_row()
-    for jj in range(0, 2):
-        cell = generate_layout_cell()
-        layout.add_to_row(cell)
+    markers, marker_list, marker_overlay = iop.layout_marker(layout_cell)
+    global_label = iop.label_global_positions(marker_list)
+    layout_cell.add_to_layer(1, markers, global_label)
 
-layout_cell, mapping = layout.generate_layout()
-
-markers, marker_list, marker_overlay = iop.layout_marker(layout_cell)
-global_label = iop.label_global_positions(marker_list)
-layout_cell.add_to_layer(1, markers, global_label)
-
-layout_cell.show()
-"""
+    layout_cell.show()
